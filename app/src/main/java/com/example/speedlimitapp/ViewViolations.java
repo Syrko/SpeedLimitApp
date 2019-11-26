95a1083f937f4de325adf98be5326a59a34150a9
Syrko <kostassir@yahoo.gr>
2019-11-26 21:53:50 +0200

    Implemented map button functionality

--- app/src/main/java/com/example/speedlimitapp/ViewViolations.java
+++ app/src/main/java/com/example/speedlimitapp/ViewViolations.java
@@ -2,7 +2,9 @@ package com.example.speedlimitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -57,7 +59,12 @@ public class ViewViolations extends AppCompatActivity {
     }
 
     private void setUpButtonListeners(){
-        // TODO implement and call maps
+        viewOnMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), ShowViolationsOnMap.class));
+            }
+        });
     }
 
     /**
